2dbac5b75f1d8a6c8fec50e781eda1e1ca107948
Max Margalith <max.margalith@dremio.com>
2023-05-08 15:44:53 +0200

    added and commented out tests

--- cmd/local_test.go
+++ cmd/local_test.go
@@ -1,52 +1,54 @@
 package cmd
 
-import (
-	"testing"
-)
+// func TestCreateAllDirs(t *testing.T) {
 
-func TestCreateAllDirs(t *testing.T) {
+// 	err := createAllDirs()
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
 
-	err := createAllDirs()
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-}
+// func TestCollectWlm(t *testing.T) {
 
-func TestCollectWlm(t *testing.T) {
+// 	err := collectWlm()
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
 
-	err := collectWlm()
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-}
+// func TestCollectKVReport(t *testing.T) {
 
-func TestCollectKVReport(t *testing.T) {
+// 	err := collectKvReport()
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
 
-	err := collectKvReport()
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-}
+// func TestCollectDremioSystemTables(t *testing.T) {
+// 	err := collectDremioSystemTables()
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
 
-func TestCollectDremioSystemTables(t *testing.T) {
-	err := collectDremioSystemTables()
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-}
+// func TestDownloadJobProfile(t *testing.T) {
+// 	jobid := "1bb5803c-5a67-d548-2547-bd180cd2fe00"
+// 	err := downloadJobProfile(jobid)
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
 
-func TestDownloadJobProfile(t *testing.T) {
-	// Requires
-	jobid := "1bb5803c-5a67-d548-2547-bd180cd2fe00"
-	err := downloadJobProfile(jobid)
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-}
+// func TestValidateApiCredentials(t *testing.T) {
+// 	err := validateApiCredentials()
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
 
-func TestValidateApiCredentials(t *testing.T) {
-	err := validateApiCredentials()
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-}
+// func TestValidateCollectJobProfiles(t *testing.T) {
+// 	err := collectJobProfiles()
+// 	if err != nil {
+// 		t.Errorf("unexpected error %v", err)
+// 	}
+// }
