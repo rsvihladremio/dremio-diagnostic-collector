0e54dd9c74f17e5a0be6494e6670f62efb398d97
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2023-05-30 19:19:30 +0200

    fix tests

--- pkg/matchers/gzip_test.go
+++ pkg/matchers/gzip_test.go
@@ -1,6 +1,8 @@
 package matchers_test
 
 import (
+	"path/filepath"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/rsvihladremio/dremio-diagnostic-collector/pkg/matchers"
@@ -9,16 +11,16 @@ import (
 var _ = Describe("Gzip Matchers", func() {
 	Context("ContainFileInGzip", func() {
 		It("should contain the expected file", func() {
-			gzipFile := "testdata/archive.tar.gz"
-			expectedFile := "file1.txt"
-
+			gzipFile := filepath.Join("testdata", "file1.txt.gz")
+			expectedFile := filepath.Join("testdata", "file1.txt")
 			// Expect the gzip file to contain the expected file
+
 			Expect(gzipFile).To(ContainThisFileInTheGzip(expectedFile))
 		})
 
 		It("should not contain a different file", func() {
-			gzipFile := "testdata/archive.tar.gz"
-			expectedFile := "file3.txt"
+			gzipFile := filepath.Join("testdata", "file1.txt.gz")
+			expectedFile := filepath.Join("testdata", "file3.txt")
 
 			// Expect the gzip file not to contain the different file
 			Expect(gzipFile).NotTo(ContainThisFileInTheGzip(expectedFile))
