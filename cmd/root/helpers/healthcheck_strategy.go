4000b1e7d4db2cc7892e8edb89d4ff94de1f5621
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2023-07-06 14:32:18 +0200

    Revert "6 support for scraping kubectl log from each pod on all containers and storing in the health check folder (#66)"
    This reverts commit 3a7564b24e42244580539bfbd0a56b398a721663.

--- cmd/root/helpers/healthcheck_strategy.go
+++ cmd/root/helpers/healthcheck_strategy.go
@@ -97,10 +97,9 @@ func (s *CopyStrategyHC) CreatePath(fileType, source, nodeType string) (path str
 	baseDir := s.BaseDir
 	tmpDir := s.TmpDir
 
-	// We only tag a suffix of '-C' / '-E' for ssh nodes, the K8s pods are desriptive enough to determine the coordinator / executor
-	// Also added in exclusion for container-logs
+	// We only tag a suffix of '-C' / '-E' for ssh nodes, the K8s pods are desriptive enough to determine the coordinator / executpr
 	var isK8s bool
-	if strings.Contains(source, "dremio-master") || strings.Contains(source, "dremio-executor") || strings.Contains(source, "dremio-coordinator") || strings.Contains(source, "container-logs") {
+	if strings.Contains(source, "dremio-master") || strings.Contains(source, "dremio-executor") || strings.Contains(source, "dremio-coordinator") {
 		isK8s = true
 	}
 	if !isK8s { // SSH node types
